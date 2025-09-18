90a65f988b6d3d0c2a4d171e474b01ba74730ac8
SourovKarmokar <sourovkarmokar22@gmail.com>
2025-09-18 18:15:07 +0600

    Products

---
+++ app/products/page.jsx
@@ -0,0 +1,28 @@
+"use client"
+
+import React from 'react'
+import {motion} from "framer-motion"
+import StatCard from '@/components/StatCard'
+import { DollarSign, ShoppingBag, SquareActivity } from 'lucide-react'
+
+const ProductsPage = () => {
+  return (
+    <div className='flex-1 overflow-auto relative x-10 '>
+      <main className='max-w-7xl mx-auto py-6 px-4 lg:px-8 '>
+        <motion.div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 mb-8 '
+        initial={{opacity: 0, y: 20}}
+        animate={{opacity:1, y:0}}
+        transition={{delay:0.3,duration:0.5}}
+        >
+            <StatCard name='Total Products' icon={ShoppingBag} value={"4352"}/>
+            <StatCard name='Total Stock' icon={SquareActivity} value={"14,450"}/>
+            <StatCard  name='Total Sold' icon={DollarSign} value={"12,450"}/>
+            <StatCard  name='Total Categories' icon={SquareActivity} value={"8"}/>
+
+        </motion.div>
+      </main>
+    </div>
+  )
+}
+
+export default ProductsPage
--- components/StatCard.jsx
+++ components/StatCard.jsx
@@ -4,20 +4,19 @@ import { motion } from "framer-motion";
 
 const StatCard = ({ name, icon: Icon, value }) => {
   return (
-  <motion.div
-    whileHover={{ y: -5, boxShadow: "0 25px 50px -12px rgb(0, 0, 0, 0.25)" }}
-    className='bg-[#1e1e1e] backdrop-blur-md overflow-hidden shadow-lg rounded-xl border border-[#1f1f1f] cursor-pointer'
-  >
-    <div className='px-4 py-5 sm:p-6'>
-      <span className='flex items-center text-sm font-medium text-gray-300'>
-        <Icon size={20} className="mr-2" />
-        {name}
-      </span>
-      <p className='mt-1 text-3xl font-semibold text-white'>{value}</p>
-    </div>
-  </motion.div>
-);
-
+    <motion.div
+      whileHover={{ y: -5, boxShadow: "0 25px 50px -12px rgb(0, 0, 0, 0.25)" }}
+      className='bg-[#1e1e1e] backdrop-blur-md overflow-hidden shadow-lg rounded-xl border border-[#1f1f1f] cursor-pointer'
+    >
+      <div className='px-4 py-5 sm:p-6'>
+        <span className='flex items-center text-sm font-medium text-gray-300'>
+          {Icon && <Icon size={20} className="mr-2" />}
+          {name}
+        </span>
+        <p className='mt-1 text-3xl font-semibold text-white'>{value}</p>
+      </div>
+    </motion.div>
+  );
 }
 
-export default StatCard
+export default StatCard;
